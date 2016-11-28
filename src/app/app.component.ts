59874d1db01a8e633135f6449a90c6db14004a53
SFujimoto <sfujimoto@MacBook-Pro.local>
2016-11-28 13:46:27 +0900

    ~#19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { initializeApp, database } from 'firebase';
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from "angularfire2";
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +11,50 @@ import { initializeApp, database } from 'firebase';
 })
 export class AppComponent {
   title = 'app works!';
+  courses$: FirebaseListObservable<any>;
+  lesson$: FirebaseObjectObservable<any>;
+  firstCourse:any;
 
-  constructor() {
-    // Initialize Firebase
-    const config = {
-      apiKey: "AIzaSyBaIJ1WsVTcm0rRwwYocZbUIZ0CvnUk6uo",
-      authDomain: "angular-firebase-91a20.firebaseapp.com",
-      databaseURL: "https://angular-firebase-91a20.firebaseio.com",
-      storageBucket: "angular-firebase-91a20.appspot.com",
-      messagingSenderId: "698647074814"
-    };
+  constructor(af: AngularFire) {
+    this.courses$ = af.database.list('courses'); // the $ at the end means that it is an observable
+    this.courses$.subscribe(console.log);
 
-    initializeApp(config);
+    this.lesson$ = af.database.object('lessons/-KX_zLkT8IBN0sPh-idV');
 
-    const root = database().ref();
+    this.lesson$.subscribe(console.log);
 
-    root.on('value', function(snap){
-      console.log(snap.val());
-    });
+    this.courses$.map(courses => courses[0])
+      .subscribe(
+        course => this.firstCourse = course
+      )
+
   }
 
+  listPush() {
+    this.courses$.push({description: 'TEST NEW COURSE'})
+      .then(
+        () => console.log('List Push Done'),
+        console.error
+      )
+  }
+
+  listRemove() {
+    this.courses$.remove(this.firstCourse)
+  }
+
+  listUpdate() {
+    this.courses$.update(this.firstCourse, {description: 'Angular2 HTTP MOD'})
+  }
+
+  objUpdate() {
+    this.lesson$.update({description: 'NEW DESCRIPTION'});
+  }
+
+  objSet() {
+    this.lesson$.set({description: 'NEW DESCRIPTION'});
+  }
+
+  objRemove() {
+    this.lesson$.remove();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "angularfire2";
+import {firebaseConfig} from "../environments/firebase.config";
 
 @NgModule({
   declarations: [
@@ -11,10 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
